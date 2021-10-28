0f91480217e02a60f531ce0e3ef9ed3212d457e9
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-28 16:02:09 -0300

    First Commit

---
+++ src/main/java/com/marcossa/api/apirestproject/ApirestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.marcossa.api.apirestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcossa/api/apirestproject/ApirestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcossa.api.apirestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
