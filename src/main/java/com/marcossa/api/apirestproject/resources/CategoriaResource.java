c50a1391df6e1b939d2b627b1460961fb2dfb376
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-28 16:38:07 -0300

    Entity Categoria and CategoriaResource update

--- src/main/java/com/marcossa/api/apirestproject/resources/CategoriaResource.java
+++ src/main/java/com/marcossa/api/apirestproject/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.marcossa.api.apirestproject.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcossa.api.apirestproject.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.addAll(Arrays.asList(c1, c2));
+		
+		return list;
 	}
 
 }
