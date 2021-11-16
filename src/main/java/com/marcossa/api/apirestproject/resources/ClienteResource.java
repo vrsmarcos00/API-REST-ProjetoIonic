5c1c47731d9b15fd00b81c70956adcc88a102fc1
vrsmarcos00 <vrr.marcos@gmail.com>
2021-11-16 19:17:37 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/marcossa/api/apirestproject/resources/ClienteResource.java
+++ src/main/java/com/marcossa/api/apirestproject/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@GetMapping
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<ClienteDTO> list = service.findAll();
@@ -59,6 +61,7 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
