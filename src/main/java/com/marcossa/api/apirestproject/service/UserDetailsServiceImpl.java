24887d34bc38302e1fb342228ceb6c4b70243a10
vrsmarcos00 <vrr.marcos@gmail.com>
2021-11-16 14:45:47 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/marcossa/api/apirestproject/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.marcossa.api.apirestproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.marcossa.api.apirestproject.domain.Cliente;
+import com.marcossa.api.apirestproject.repositories.ClienteRepository;
+import com.marcossa.api.apirestproject.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfil());
+	}
+
+}
